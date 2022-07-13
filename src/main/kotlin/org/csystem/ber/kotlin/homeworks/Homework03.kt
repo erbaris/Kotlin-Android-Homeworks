c4e75135bb6b721d452588dcc08fa756a7f33d2f
erbaris <erbaris@yahoo.com>
2022-07-13 16:07:25 +0300

    Homework 1 and 2 Solutions

---
+++ src/main/kotlin/org/csystem/ber/kotlin/homeworks/Homework03.kt
@@ -0,0 +1,54 @@
+package org.csystem.ber.kotlin.homeworks
+
+fun main() {
+    while (true) {
+        print("Select Solution Number of 5 (0 for exit):")
+        val n = readLine()!!.toInt()
+        when (n) {
+            0 -> break
+            1 -> HW03_1_run()
+            2 -> HW03_2_run()
+            3 -> HW03_3_run()
+            4 -> HW03_4_run()
+            5 -> HW03_5_run()
+            6 -> HW03_6_run()
+            7 -> HW03_7_run()
+            8 -> HW03_8_run()
+            else -> break
+
+        }
+    }
+}
+
+fun HW03_1_run()
+{
+    TODO()
+}
+fun HW03_2_run()
+{
+    TODO()
+}
+fun HW03_3_run()
+{
+    TODO()
+}
+fun HW03_4_run()
+{
+    TODO()
+}
+fun HW03_5_run()
+{
+    TODO()
+}
+fun HW03_6_run()
+{
+    TODO()
+}
+fun HW03_7_run()
+{
+    TODO()
+}
+fun HW03_8_run()
+{
+    TODO()
+}
